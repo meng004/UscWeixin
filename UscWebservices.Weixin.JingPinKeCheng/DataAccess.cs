93060ec4639fd15ddda05842c0df193cd74c1025
mlemon <meng004@gmail.com>
2014-05-28 10:56:20 +0800

    精品课程数据访问修改为code first

--- UscWebservices.Weixin.JingPinKeCheng/DataAccess.cs
+++ UscWebservices.Weixin.JingPinKeCheng/DataAccess.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using UscWebservices.Weixin.IServices;
 
 namespace UscWebservices.Weixin.JingPinKeCheng
 {
-    public class DataAccess 
+    public class DataAccess : IFindPwd
     {
         /// <summary>
         /// 由登录用户名，取密码
@@ -14,10 +14,10 @@ namespace UscWebservices.Weixin.JingPinKeCheng
         /// <returns></returns>
         public string GetPasswordByUsername(string username)
         {
-            using (var context = new JingPinKeChengEntities())
+            using (var context = new JingPinKeChengDbContext())
             {
                 //只接受学生帐号
-                var user = context.user.First(t =>
+                var user = context.Users.First(t =>
                     t.logname == username && t.user_type == 2
                     );
                 //处理帐号错误
@@ -29,7 +29,6 @@ namespace UscWebservices.Weixin.JingPinKeCheng
                 {
                     return user.password_src;
                 }
-
             }
         }
     }
